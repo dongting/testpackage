8783ad82d504f5628894777167f2db3fe9d7c7a4
Dongting Yu <dongtingyu@gmail.com>
2022-09-24 17:46:04 -0700

    Add network functions

--- testpackage/package.py
+++ testpackage/package.py
@@ -1,4 +1,5 @@
 import sys
+import socket
 
 from . import helper
 
@@ -12,16 +13,49 @@ def read_file(filename):
 
 
 def tcp_out():
-    pass
+    HOST = '127.0.0.1'
+    PORT = 8888
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(b"Hello, world")
+        # data = s.recv(1024)
+        # print(f"Received {data!r}")
 
 
 def tcp_in():
-    pass
+    HOST = '127.0.0.1'
+    PORT = 8888
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            # print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
 
 
 def udp_out():
-    pass
+    UDP_IP = '127.0.0.1'
+    UDP_PORT = 8888
+    MESSAGE = b'Hello, World!'
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
 
 def udp_in():
-    pass
+    UDP_IP = '127.0.0.1'
+    UDP_PORT = 8888
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((UDP_IP, UDP_PORT))
+
+    while True:
+        data, addr = sock.recvfrom(1024)
+        # print("received message: %s" % data)
